d3383c61b6c577c87e2b2f9d7a07e4c074af61f0
YukiMaitani <fightzoi1997@gmail.com>
2023-09-23 10:21:45 +0900

    ViewModelでリポジトリを取得する書き方をきれいにする

--- lib/ui/pokemon/pokemon_view_model.dart
+++ lib/ui/pokemon/pokemon_view_model.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:okina_honban/data/model/model.dart';
+import 'package:okina_honban/data/repository/pokemon_repository.dart';
 import 'package:okina_honban/ui/base/base.dart';
 
 import '../../data/repository/pokemon_rempository_impl.dart';
@@ -12,6 +13,9 @@ final pokemonViewModelProvider =
 class PokemonViewModel extends BaseViewModel {
   PokemonViewModel(super.ref);
 
+  PokemonRepository get _pokemonRepository =>
+      ref.read(pokemonRepositoryProvider);
+
   Result<Pokemon>? _pokemonResult;
 
   Result<Pokemon>? get pokemonResult => _pokemonResult;
@@ -20,8 +24,7 @@ class PokemonViewModel extends BaseViewModel {
 
   Future<void> getPokemon() async {
     final id = Random().nextInt(1011);
-    _pokemonResult = await callFuture(
-        () => ref.read(pokemonRepositoryProvider).getPokemon(id));
+    _pokemonResult = await callFuture(() => _pokemonRepository.getPokemon(id));
     notifyListeners();
   }
 }
