339ab6f7a974ef31c38b3eda3edf8adcbc3f3706
fen0268 <Kenshi150107@gmail.com>
2023-09-30 20:47:16 +0900

    feat: ホーム画面のベースを作成

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:okina_honban/router/router_path.dart';
 import 'package:okina_honban/ui/base/base.dart';
 import 'package:okina_honban/ui/home/home_view_model.dart';
 
@@ -17,17 +19,37 @@ class HomePage extends HookConsumerWidget {
             icon: const Icon(Icons.logout),
           )
         ]),
-        body: _buildBody());
+        body: _buildBody(context));
   }
 
-  Widget _buildBody() {
+  Widget _buildBody(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [_buildText()],
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildText(),
+        _buildStartButton(),
+        _buildRankingButton(context),
+      ],
     );
   }
 
   Widget _buildText() {
-    return const Center(child: Text('ホーム'));
+    return const Center(child: Text('TOEIC 対策'));
+  }
+
+  Widget _buildStartButton() {
+    return ElevatedButton(
+      onPressed: () {},
+      child: const Text('スタート'),
+    );
+  }
+
+  Widget _buildRankingButton(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        context.push(rankingPath);
+      },
+      child: const Text('ランキング'),
+    );
   }
 }
