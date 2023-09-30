134dd52dd178f08ac8c91222c95febbb2062f577
fen0268 <Kenshi150107@gmail.com>
2023-10-01 04:16:08 +0900

    feat: リポジトリから supabase のデータを取得

--- lib/ui/ranking/ranking_view_model.dart
+++ lib/ui/ranking/ranking_view_model.dart
@@ -1,4 +1,6 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:okina_honban/data/model/model.dart';
+import 'package:okina_honban/data/repository/repository.dart';
 
 import '../base/base.dart';
 
@@ -6,5 +8,17 @@ final rankingViewModelProvider =
     ChangeNotifierProvider.autoDispose(RankingViewModel.new);
 
 class RankingViewModel extends BaseViewModel {
-  RankingViewModel(super.ref);
+  RankingViewModel(super.ref) {
+    init();
+    notifyListeners();
+  }
+  List<TetoeicUser>? users;
+
+  Future<void> init() async {
+    final usersResult = await callFuture<List<TetoeicUser>>(
+        () async => ref.read(supabaseRepositoryProvider).getScores());
+
+    users = usersResult.data;
+    notifyListeners();
+  }
 }
