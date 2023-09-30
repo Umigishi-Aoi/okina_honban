d8db764044e1e6b4d3ab1deb68caa37a11a21d0b
fen0268 <Kenshi150107@gmail.com>
2023-09-30 22:05:45 +0900

    refactor: widget を切り分け

--- lib/ui/ranking/ranking_page.dart
+++ lib/ui/ranking/ranking_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../router/router_path.dart';
 import '../base/base_page.dart';
 
 class RankingPage extends HookConsumerWidget {
@@ -17,29 +19,49 @@ class RankingPage extends HookConsumerWidget {
         return Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('ランキング'),
-            Container(
-              height: 400,
-              margin: const EdgeInsets.all(16),
-              padding: const EdgeInsets.only(top: 10, bottom: 10),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: ListView.builder(
-                itemCount: 50,
-                itemBuilder: (context, index) {
-                  return Card(
-                    child: ListTile(
-                      leading: Text('${index + 1}'),
-                      title: const Text('User_Name'),
-                      trailing: const Text('Score'),
-                    ),
-                  );
-                },
-              ),
-            ),
+            _buildText(),
+            _buildRankingList(),
+            _buildBackButton(),
           ],
+        );
+      },
+    );
+  }
+
+  Widget _buildText() {
+    return const Center(child: Text('ランキング'));
+  }
+
+  Widget _buildRankingList() {
+    return Container(
+      height: 400,
+      margin: const EdgeInsets.all(16),
+      padding: const EdgeInsets.only(top: 10, bottom: 10),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: ListView.builder(
+        itemCount: 50,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              leading: Text('${index + 1}'),
+              title: const Text('User_Name'),
+              trailing: const Text('Score'),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildBackButton() {
+    return Builder(
+      builder: (context) {
+        return ElevatedButton(
+          onPressed: () => context.go(homePath),
+          child: const Text('戻る'),
         );
       },
     );
