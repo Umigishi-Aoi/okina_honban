1b76e09afa95ee3e1e05f75c9882a0fe9e5f2543
fen0268 <Kenshi150107@gmail.com>
2023-09-30 18:22:49 +0900

    feat: ランキングページのパスを追加

--- lib/router/router_path.dart
+++ lib/router/router_path.dart
@@ -3,3 +3,4 @@ const String signInPath = '/sign_in';
 const String signUpPath = '/sign_up';
 const String loadingPath = '/loading';
 const String errorPath = '/error';
+const String rankingPath = '/ranking';
