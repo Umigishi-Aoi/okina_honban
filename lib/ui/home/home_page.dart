2aa3f70d44807d4ac25be8091bac17ddacd0541c
fen0268 <Kenshi150107@gmail.com>
2023-09-30 22:06:34 +0900

    refactor: フォーマット整理

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:okina_honban/router/router_path.dart';
-import 'package:okina_honban/ui/base/base.dart';
-import 'package:okina_honban/ui/home/home_view_model.dart';
+
+import '../../router/router_path.dart';
+import '../base/base.dart';
+import 'home_view_model.dart';
 
 class HomePage extends HookConsumerWidget {
   const HomePage({super.key});
@@ -11,15 +12,18 @@ class HomePage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return BasePage(
-        appBar: BaseAppBar(actions: [
+      appBar: BaseAppBar(
+        actions: [
           IconButton(
             onPressed: () async {
               await ref.watch(homeViewModelProvider).signOut();
             },
             icon: const Icon(Icons.logout),
-          )
-        ]),
-        body: _buildBody());
+          ),
+        ],
+      ),
+      body: _buildBody(),
+    );
   }
 
   Widget _buildBody() {
@@ -37,23 +41,25 @@ class HomePage extends HookConsumerWidget {
     return const Center(child: Text('TOEIC 対策'));
   }
 
-  Widget _buildRankingButton() {
-    return Builder(builder: (context) {
-      return ElevatedButton(
-        onPressed: () {
-          context.push(rankingPath);
-        },
-        child: const Text('ランキング'),
-      );
-    });
+  Widget _buildGoToGamePageButton() {
+    return Builder(
+      builder: (context) {
+        return ElevatedButton(
+          onPressed: () => context.go(gamePath),
+          child: const Text('テトリス'),
+        );
+      },
+    );
   }
 
-  Widget _buildGoToGamePageButton() {
-    return Builder(builder: (context) {
-      return ElevatedButton(
-        onPressed: () => context.go(gamePath),
-        child: const Text('テトリス'),
-      );
-    });
+  Widget _buildRankingButton() {
+    return Builder(
+      builder: (context) {
+        return ElevatedButton(
+          onPressed: () => context.go(rankingPath),
+          child: const Text('ランキング'),
+        );
+      },
+    );
   }
 }
