7437d1bc2ccdeb97fcf3c28ac3193045cfb1fb76
fen0268 <Kenshi150107@gmail.com>
2023-10-01 11:20:03 +0900

    refactor: レスポンシブ対応

--- lib/ui/sign_in/sign_in_page.dart
+++ lib/ui/sign_in/sign_in_page.dart
@@ -12,7 +12,11 @@ class SignInPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return BasePage(body: _buildBody());
+    return BasePage(
+        appBar: const BaseAppBar(
+          showBackButton: false,
+        ),
+        body: _buildBody());
   }
 
   Widget _buildBody() {
@@ -21,64 +25,148 @@ class SignInPage extends HookConsumerWidget {
     final isDisplay = useState(true);
     return HookConsumer(
       builder: (context, ref, __) {
-        return Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(8, 100, 8, 8),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  label: Text('メールアドレス'),
-                ),
-                controller: emailController,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  suffixIcon: isDisplay.value == true
-                      ? IconButton(
-                          icon: const Icon(Icons.visibility),
-                          onPressed: () {
-                            isDisplay.value = !isDisplay.value;
-                          },
-                        )
-                      : IconButton(
-                          icon: const Icon(Icons.visibility_off),
-                          onPressed: () {
-                            isDisplay.value = !isDisplay.value;
-                          },
+        return LayoutBuilder(
+          builder: (context, constraints) {
+            if (constraints.maxWidth < 600) {
+              return Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8, 100, 8, 8),
+                    child: SizedBox(
+                      width: constraints.maxWidth * 0.8,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          label: Text('メールアドレス'),
                         ),
-                  border: const OutlineInputBorder(),
-                  label: const Text('パスワード'),
-                ),
-                controller: passwordController,
-                obscureText: isDisplay.value,
+                        controller: emailController,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: constraints.maxWidth * 0.8,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          suffixIcon: isDisplay.value == true
+                              ? IconButton(
+                                  icon: const Icon(Icons.visibility),
+                                  onPressed: () {
+                                    isDisplay.value = !isDisplay.value;
+                                  },
+                                )
+                              : IconButton(
+                                  icon: const Icon(Icons.visibility_off),
+                                  onPressed: () {
+                                    isDisplay.value = !isDisplay.value;
+                                  },
+                                ),
+                          border: const OutlineInputBorder(),
+                          label: const Text('パスワード'),
+                        ),
+                        controller: passwordController,
+                        obscureText: isDisplay.value,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {}, child: const Text('パスワードをお忘れですか？')),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        await ref.read(signInViewModelProvider.notifier).signIn(
+                              email: emailController.text,
+                              password: passwordController.text,
+                            );
+                        if (!context.mounted) {
+                          return;
+                        }
+                        context.go(homePath);
+                      },
+                      child: const Text('サインイン'),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () => context.go(signUpPath),
+                    child: const Text('アカウント作成'),
+                  ),
+                ],
+              );
+            }
+            return SizedBox(
+              width: constraints.maxWidth,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8, 100, 8, 8),
+                    child: SizedBox(
+                      width: 500,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          label: Text('メールアドレス'),
+                        ),
+                        controller: emailController,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SizedBox(
+                      width: 500,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          suffixIcon: isDisplay.value == true
+                              ? IconButton(
+                                  icon: const Icon(Icons.visibility),
+                                  onPressed: () {
+                                    isDisplay.value = !isDisplay.value;
+                                  },
+                                )
+                              : IconButton(
+                                  icon: const Icon(Icons.visibility_off),
+                                  onPressed: () {
+                                    isDisplay.value = !isDisplay.value;
+                                  },
+                                ),
+                          border: const OutlineInputBorder(),
+                          label: const Text('パスワード'),
+                        ),
+                        controller: passwordController,
+                        obscureText: isDisplay.value,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {}, child: const Text('パスワードをお忘れですか？')),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        await ref.read(signInViewModelProvider.notifier).signIn(
+                              email: emailController.text,
+                              password: passwordController.text,
+                            );
+                        if (!context.mounted) {
+                          return;
+                        }
+                        context.go(homePath);
+                      },
+                      child: const Text('サインイン'),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () => context.go(signUpPath),
+                    child: const Text('アカウント作成'),
+                  ),
+                ],
               ),
-            ),
-            TextButton(onPressed: () {}, child: const Text('パスワードをお忘れですか？')),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                onPressed: () async {
-                  await ref.read(signInViewModelProvider.notifier).signIn(
-                        email: emailController.text,
-                        password: passwordController.text,
-                      );
-                  if (!context.mounted) {
-                    return;
-                  }
-                  context.go(homePath);
-                },
-                child: const Text('サインイン'),
-              ),
-            ),
-            TextButton(
-              onPressed: () => context.go(signUpPath),
-              child: const Text('アカウント作成'),
-            ),
-          ],
+            );
+          },
         );
       },
     );
